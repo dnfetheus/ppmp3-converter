a8a55641ca50de84e34b157eb205d22f5e5416d3
Felipe Matheus <fmalbernaz@protonmail.com>
2019-05-15 22:51:33 -0300

    Refactor ppmp3.c

--- include/ppmp3.h
+++ include/ppmp3.h
@@ -2,12 +2,13 @@
 #define PPMP3_H
 
 typedef struct ppmp3_image* ppmp3_image;
+typedef enum ppmp3_rotation ppmp3_rotation;
 
-ppmp3_image load_image(char path[]);
-void free_image(ppmp3_image image);
-void write_image(ppmp3_image image, char destination[]);
-ppmp3_image* grayscale(ppmp3_image image);
-ppmp3_image* rotate(ppmp3_image *image, unsigned short mode);
-ppmp3_image* change_brightness(ppmp3_image *image, float percent);
+ppmp3_image ppm3_load_image(char path[]);
+void ppmp3_free_image(ppmp3_image image);
+void ppmp3_write_image(ppmp3_image image, char destination[]);
+ppmp3_image ppmp3_grayscale(ppmp3_image image);
+ppmp3_image ppmp3_rotate(ppmp3_image image, ppmp3_rotation mode);
+ppmp3_image ppmp3_change_brightness(ppmp3_image image, float percent);
 
 #endif
--- src/ppmp3.c
+++ src/ppmp3.c
@@ -1,18 +1,23 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "../include/ppmp3.h"
 
-struct pixel{
+typedef struct pixel{
     unsigned char r;
     unsigned char g;
     unsigned char b;
-};
+} pixel;
 
 struct ppmp3_image{
     unsigned int width;
     unsigned int height;
     unsigned char maximum;
     pixel **pixels;
+};
+
+enum ppmp3_rotation{
+	Clockwise, Reverse, Counterclockwise
 };
 
 pixel** ppmp3_create_pixels(unsigned int width, unsigned int height){
@@ -22,10 +27,10 @@ pixel** ppmp3_create_pixels(unsigned int width, unsigned int height){
 		exit(255);
 	}
 
-	for(int i = 0; i < height; i++){
+	for(unsigned int i = 0; i < height; i++){
 		pixels[i] = malloc(width * sizeof(pixel));
 
-		if (pixels[i] == NULL){
+		if(pixels[i] == NULL){
 			exit(255);
 		}
 	}
@@ -33,148 +38,156 @@ pixel** ppmp3_create_pixels(unsigned int width, unsigned int height){
 	return pixels;
 }
 
-void load_pixels(ppm_p3_image *image, FILE *file){
-	int i, j;
-	for (i = 0; i < image->height; i++){
-		for (j = 0; j < image->width; j++){
+void ppmp3_load_pixels(ppmp3_image image, FILE *file){
+	for(unsigned int i = 0; i < image->height; i++){
+		for(unsigned int j = 0; j < image->width; j++){
 			fscanf(file, "%d %d %d", &image->pixels[i][j].r, &image->pixels[i][j].g, &image->pixels[i][j].b);
 		}
 	}
 }
 
-ppm_p3_image* create_image(int width, int height, unsigned char maximum, pixel **pixels){
-	ppm_p3_image *image = (ppm_p3_image*) malloc(sizeof(ppm_p3_image));
-	if (image == NULL){
-		printf("Error on memory allocation");
+ppmp3_image ppmp3_create_image(unsigned int width, unsigned int height, unsigned char maximum, pixel **pixels){
+	ppmp3_image image = malloc(sizeof(ppmp3_image));
+
+	if(image == NULL){
 		exit(255);
 	}
+
 	image->height = height;
 	image->width = width;
 	image->maximum = maximum;
 	image->pixels = pixels;
+
 	return image;
 }
 
-ppm_p3_image* load_image(char name[]){
-	FILE *file = fopen(name, "r");
-	if (file == NULL){
-		printf("Cannot be able to open the file\n");
+ppmp3_image ppmp3_load_image(char path[]){
+	FILE *file = fopen(path, "r");
+
+	if(file == NULL){
 		return NULL;
 	}
-	int w, h, max;
+
+	unsigned int width, height, max;
 	char type[3];
-	fscanf(file, "%s\n%d %d\n%d", &type, &w, &h, &max);
-	if (strcmp(type, "P3") != 0){
-		printf("The file isn't a PPM P3 image\n");
+
+	fscanf(file, "%s\n%d %d\n%d", &type, &width, &height, &max);
+
+	if(strcmp(type, "P3") != 0){
 		return NULL;
 	}
-	pixel **pixels = create_pixels(w, h);
-	ppm_p3_image *image = create_image(w, h, max, pixels);
-	load_pixels(image, file);
+
+	pixel **pixels = ppmp3_create_pixels(width, height);
+	ppmp3_image image = ppmp3_create_image(width, height, max, pixels);
+
+	ppmp3_load_pixels(image, file);
 	fclose(file);
+
 	return image;
 }
 
-void free_image(ppm_p3_image *image){
-	int i;
-	for (i = 0; i < image->height; i++){
+void ppmp3_free_image(ppmp3_image image){
+	for(unsigned int i = 0; i < image->height; i++){
 		free(image->pixels[i]);
 	}
+
 	free(image->pixels);
 	free(image);
 }
 
-void write_image(ppm_p3_image *image, char name[]){
-	FILE *file = fopen(name, "w");
-	if (file == NULL){
-		printf("File cannot be saved\n");
+void ppmp3_write_image(ppmp3_image image, char destination[]){
+	FILE *file = fopen(destination, "w");
+
+	if(file == NULL){
+		return;
 	}
+
 	fprintf(file, "P3\n%d %d\n%d\n", image->width, image->height, image->maximum);
-	int i, j;
-	for (i = 0; i < image->height; i++){
-		for(j = 0; j < image->width; j++){
-			if (j == image->width - 1)
+
+	for(unsigned int i = 0; i < image->height; i++){
+		for(unsigned int j = 0; j < image->width; j++){
+			if(j == image->width - 1){
 				fprintf(file, "%d %d %d\n", image->pixels[i][j].r, image->pixels[i][j].g, image->pixels[i][j].b);
-			else
+			}
+
+			else{
 				fprintf(file, "%d %d %d ", image->pixels[i][j].r, image->pixels[i][j].g, image->pixels[i][j].b);
+			}
 		}
 	}
-	free_image(image);
+
+	ppmp3_free_image(image);
 	fclose(file);
 }
 
-ppm_p3_image* grayscale(ppm_p3_image *image){
-	int i, j;
-	ppm_p3_image *new = create_image(image->width, image->height, 0, create_pixels(image->width, image->height));
-	for (i = 0; i < new->height; i++){
-		for (j = 0; j < new->width; j++){
-			unsigned char average_gray = (int) (image->pixels[i][j].r * 0.3) + (image->pixels[i][j].g * 0.59) + (image->pixels[i][j].b * 0.11);
-			new->pixels[i][j].r = average_gray;
-			new->pixels[i][j].g = average_gray;
-			new->pixels[i][j].b = average_gray;
-			if (average_gray > new->maximum)
-				new->maximum = average_gray;
+ppmp3_image ppmp3_grayscale(ppmp3_image image){
+	ppmp3_image new_image = ppmp3_create_image(image->width, image->height, 0, create_pixels(image->width, image->height));
+	unsigned char average_gray;
+
+	for(unsigned int i = 0; i < new_image->height; i++){
+		for (unsigned int j = 0; j < new_image->width; j++){
+			average_gray = (int) (image->pixels[i][j].r * 0.3) + (image->pixels[i][j].g * 0.59) + (image->pixels[i][j].b * 0.11);
+
+			if(average_gray > new_image->maximum){
+				new_image->maximum = average_gray;
+			}
+
+			new_image->pixels[i][j].r = new_image->pixels[i][j].g = new_image->pixels[i][j].b = average_gray;
 		}
 	}
-	return new;
+	return new_image;
 }
 
-ppm_p3_image* rotate(ppm_p3_image *image, int mode){
-	ppm_p3_image *new;
+ppmp3_image ppmp3_rotate(ppmp3_image image, enum ppmp3_rotation mode){
 	pixel **pixels;
-	int i, j, width, height;
-	switch(mode){
-		case 1:
-			width = image->height;
-			height = image->width;
-			pixels = create_pixels(width, height);
-			for (i = 0; i < width; i++){
-				for (j = 0; j < height; j++){
-					pixels[j][width - i - 1] = image->pixels[i][j];
-				}
-			}
-			break;
-		case 2:
-			width = image->width;
-			height = image->height;
-			pixels = create_pixels(width, height);
-			for (i = 0; i < height; i++){
-				for (j = 0; j < width; j++){
-					pixels[height - i - 1][width - j - 1] = image->pixels[i][j];
-				}
-			}
-			break;
-		case 3:
-			width = image->height;
-			height = image->width;
-			pixels = create_pixels(width, height);
-			for (i = 0; i < width; i++){
-				for (j = 0; j < height; j++){
-					pixels[height - j - 1][i] = image->pixels[i][j];
-				}
-			}
-			break;
-		default:
-			return NULL;
-	}
-	new = create_image(width, height, image->maximum, pixels);
-	return new;
-}
+	ppmp3_image new_image;
 
-ppm_p3_image* change_brightness(ppm_p3_image *image, float percent){
-	if(percent == 100.0)
-		return image;
-	float realpercent = percent/100;
-	int i, j;
-	ppm_p3_image *new;
-	pixel **pixels = create_pixels(image->width, image->height);
-	for (i = 0; i < image->height; i++){
-		for (j = 0; j < image->width; j++){
-			pixels[i][j].r = (int) image->pixels[i][j].r * realpercent <= 255 ? image->pixels[i][j].r * realpercent : 255;
-			pixels[i][j].g = (int) image->pixels[i][j].g * realpercent <= 255 ? image->pixels[i][j].g * realpercent : 255;
-			pixels[i][j].b = (int) image->pixels[i][j].b * realpercent <= 255 ? image->pixels[i][j].b * realpercent : 255;
+	if(mode == Clockwise || mode == Counterclockwise){
+		pixels = ppmp3_create_pixels(image->height, image->width);
+		new_image = ppmp3_create_image(image->height, image->width, image->maximum, pixels);
+
+		for (unsigned int i = 0, y; i < new_image->width; i++){
+			for (unsigned int j = 0, z; j < new_image->height; j++){
+
+				y = mode == Clockwise ? j : new_image->height - j - 1;
+				z = mode == Clockwise ? new_image->width - i - 1 : i;
+
+				new_image->pixels[y][z] = image->pixels[i][j];
+			}
 		}
 	}
-	new = create_image(image->width, image->height, image->maximum, pixels);
-	return new;
+
+	else if(mode == Reverse){
+		pixels = ppmp3_create_pixels(image->width, image->height);
+		new_image = ppmp3_create_image(image->width, image->height, image->maximum, pixels);
+
+		for (unsigned int i = 0; i < new_image->height; i++){
+			for (unsigned int j = 0; j < new_image->width; j++){
+				new_image->pixels[new_image->height - i - 1][new_image->width - j - 1] = image->pixels[i][j];
+			}
+		}
+	}
+
+	else{
+		return NULL;
+	}
+
+	return new_image;
+}
+
+ppmp3_image ppmp3_change_brightness(ppmp3_image image, float percent){
+	pixel **pixels = ppmp3_create_pixels(image->width, image->height);
+	ppmp3_image new_image = ppmp3_create_image(image->width, image->height, image->maximum, pixels);
+
+	percent = percent/100;
+
+	for (unsigned int i = 0; i < new_image->height; i++){
+		for (unsigned int j = 0; j < new_image->width; j++){
+			new_image->pixels[i][j].r = (int) image->pixels[i][j].r * percent < 255 ? image->pixels[i][j].r * percent : 255;
+			new_image->pixels[i][j].g = (int) image->pixels[i][j].g * percent < 255 ? image->pixels[i][j].g * percent : 255;
+			new_image->pixels[i][j].b = (int) image->pixels[i][j].b * percent < 255 ? image->pixels[i][j].b * percent : 255;
+		}
+	}
+
+	return new_image;
 }
